129f720ef29782866b8b517fec3471bff301e713
DenysBabin <d.babin2173@gmail.com>
2023-03-03 15:58:24 +0100

    Calculator start

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,16 @@ class MyFirstApp extends StatefulWidget {
 }
 
 class _MyFirstAppState extends State<MyFirstApp> {
-  late bool _loading;
+  String string = "0";
+  String result = "0";
+  double? w1 = 85;
+  double? w2 = 170;
   late double _progressValue;
 
   @override
   void initState() {
-    _loading = false;
-    _progressValue = 0;
+    string = '0';
+    result = "0";
     super.initState();
   }
 
@@ -30,58 +33,111 @@ class _MyFirstAppState extends State<MyFirstApp> {
       home: Scaffold(
         backgroundColor: Colors.deepOrange[400],
         appBar: AppBar(
-          title: Text("My First App"),
+          title: Text("Calculator"),
           centerTitle: true,
         ),
         body: Center(
-          child: Stack(
-            fit: StackFit.expand,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Image(
-                image: AssetImage('assets/images/image-1.jpeg'),
-              ),
-              Image.asset('assets/icons/icon.png'),
-              Positioned(
-                top: 30,
-                left: 100,
-                child: Text(
-                  'My custom font',
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.amber,
-                    fontFamily: 'EBGaramond',
+              Container(
+                height: 80,
+                child: Center(
+                  child: Text(
+                    string,
+                    style: TextStyle(
+                      fontSize: 48,
+                    ),
                   ),
                 ),
+              ),
+              Container(
+                height: 80,
+                child: Center(
+                  child: Text(
+                    result,
+                    style: TextStyle(
+                      fontSize: 48,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      MyButton(myContext: "AC"),
+                      MyButton(myContext: "7"),
+                      MyButton(myContext: "4"),
+                      MyButton(myContext: "1"),
+                      MyButton(myContext: "."),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      MyButton(myContext: "<-"),
+                      MyButton(myContext: "8"),
+                      MyButton(myContext: "5"),
+                      MyButton(myContext: "2"),
+                      MyButton(myContext: "0"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      MyButton(myContext: "/"),
+                      MyButton(myContext: "9"),
+                      MyButton(myContext: "6"),
+                      MyButton(myContext: "3"),
+                      MyButton(myContext: "00"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      MyButton(myContext: "*"),
+                      MyButton(myContext: "+"),
+                      MyButton(myContext: "-"),
+                      MyButton(myContext: "=", w2: 176),
+                    ],
+                  ),
+                ],
               ),
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.cloud_download),
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-        ),
       ),
     );
   }
+}
 
-  void _updateProgress() {
-    const oneSec = const Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer timer) {
-      setState(() {
-        _progressValue += 0.2;
+class MyButton extends StatelessWidget {
+  final double? w1;
+  final double? w2;
+  final String myContext;
+  final Color myColor;
 
-        if (_progressValue.toStringAsFixed(1) == '1.0') {
-          _loading = false;
-          timer.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
-    });
+  const MyButton(
+      {Key? key,
+      this.w1 = 80,
+      this.w2 = 80,
+      required this.myContext,
+      this.myColor = Colors.white70})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: Container(
+        height: w2,
+        width: w1,
+        decoration: BoxDecoration(color: myColor),
+        child: Center(
+          child: Text(
+            myContext,
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+      ),
+    );
   }
 }
